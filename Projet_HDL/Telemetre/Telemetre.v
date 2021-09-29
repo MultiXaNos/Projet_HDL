8185a0a6469cd4cc88b3b8b8114f40a59ae82f7d
MultiXaNos <maxencedraudpro@gmail.com>
2021-09-29 22:46:35 +0200

    Add updates on module names
    Nom du spliter modifié et nettoyage dans le dossier avec uniquement les éléments nécessaires

--- Projet_HDL/Telemetre/Telemetre.v
+++ Projet_HDL/Telemetre/Telemetre.v
@@ -9,13 +9,13 @@ wire [7:0] Dist, DistMemorise;
 wire [3:0] ValA1, ValA2, ValA3;
 
 FreqDividerTL #(7350, 3675, 1, 13) Diviseur(Clk, ClkOut) ;
-CptNBitsNatTL #(8, 256, 0, 1, 0) CntDist(ClkOut, Dist, 1, Gate);
+CptNBitsNat #(8, 256, 0, 1, 0) CntDist(ClkOut, Dist, 1, Gate);
 Memoire #(8, 0) Mem(Dist, Gate, DistMemorise);
 ComparateurTL #(15, 8) CompDist(DistMemorise, Led);
-SplitByDecTL Spliter(DistMemorise, ValA1, ValA2, ValA3);
+Split8BitsBy3 Spliter(DistMemorise, ValA1, ValA2, ValA3);
 Aff7Seg #(0) Aff1(ValA1, A1);
 Aff7Seg #(0) Aff2(ValA2, A2);
-Aff7Seg #(0) Aff3(ValA2, A3);
+Aff7Seg #(0) Aff3(ValA3, A3);
 
 
 endmodule
--- Projet_HDL/Telemetre_DE2115/Telemetre.v
+++ Projet_HDL/Telemetre_DE2115/Telemetre.v
@@ -1,7 +1,7 @@
 module Telemetre(Clk, Gate, A1, A2, A3, Led);
 
 input Clk, Gate;
-output[0:6] A1, A2, A3;
+output[6:0] A1, A2, A3;
 output Led;
 
 wire ClkOut;
@@ -9,10 +9,10 @@ wire [7:0] Dist, DistMemorise;
 wire [3:0] ValA1, ValA2, ValA3;
 
 FreqDividerTL #(7350, 3675, 1, 13) Diviseur(Clk, ClkOut) ;
-CptNBitsNatTL #(8, 256, 0, 1, 0) CntDist(ClkOut, Dist, 1, Gate);
+CptNBitsNat #(8, 256, 0, 1, 0) CntDist(ClkOut, Dist, 1, Gate);
 Memoire #(8, 0) Mem(Dist, Gate, DistMemorise);
-ComparateurTL #(15, 8, 0) CompDist(DistMemorise, Led);
-SplitByDecTL Spliter(DistMemorise, ValA1, ValA2, ValA3);
+ComparateurTL #(15, 8) CompDist(DistMemorise, Led);
+Split8BitsBy3 Spliter(DistMemorise, ValA1, ValA2, ValA3);
 Aff7Seg #(0) Aff1(ValA1, A1);
 Aff7Seg #(0) Aff2(ValA2, A2);
 Aff7Seg #(0) Aff3(ValA3, A3);
